0c960120f074b37d2474680762cf5d71c17058e8
Dev Rook <110010478+Dev-Rook@users.noreply.github.com>
2022-10-19 00:13:38 -0500

    Bregoli

--- src/Components/Social-Icon-Column/SocialIconColumn.jsx
+++ src/Components/Social-Icon-Column/SocialIconColumn.jsx
@@ -8,9 +8,15 @@ import TwitterIcon from "@mui/icons-material/Twitter";
 const SocialIconColumn = () => {
   return (
     <div className={Styles.IconBar}>
-      <GitHubIcon sx={{ fontSize: "20"}} />
-      <LinkedInIcon sx={{ fontSize: "20"}} />
-      <TwitterIcon sx={{ fontSize: "20"}} />
+      <a href="https://github.com/Dev-Rook" target={"_blank"} rel={"noreferrer"}>
+        <GitHubIcon sx={{ fontSize: "20" }} />
+      </a>
+      <a href="https://www.linkedin.com/in/dev-rook/" target={"_blank"} rel={"noreferrer"}>
+        <LinkedInIcon sx={{ fontSize: "20" }} />
+      </a>
+      <a href="https://twitter.com/Dev_Rook" target={"_blank"} rel={"noreferrer"}>
+        <TwitterIcon sx={{ fontSize: "20" }} />
+      </a>
     </div>
   );
 };
