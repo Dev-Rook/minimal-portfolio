85ca2e6f0ca21bb3049df0f9d70b352648105922
Dev Rook <110010478+Dev-Rook@users.noreply.github.com>
2022-10-20 23:01:47 -0500

    Project Card template

--- src/Page-Sections/Portfolio.jsx
+++ src/Page-Sections/Portfolio.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import Styles from "../Styles/Page-Section-Styles/Portfolio/Portfolio.module.css";
 
-import CodeIcon from "@mui/icons-material/Code";
-import HttpIcon from "@mui/icons-material/Http";
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LaunchIcon from '@mui/icons-material/Launch';
 
 import ProjectData from "../Components/Data/ProjectData.json";
 
@@ -11,12 +11,10 @@ const Portfolio = () => {
 
   return (
     <div className={Styles.Section}>
-      Portfolio
+      <h3 className={Styles.Title}>Some Things I have Built</h3>
       <div className={Styles.Project_Wrapper}>
         {data &&
           data.map((value) => {
-            const { id, Image, Title, Description, Stacks, Links } = value;
-
             return (
               <div className={Styles.Project_Card}>
                 <div className={Styles.Image_Container}>
@@ -28,23 +26,23 @@ const Portfolio = () => {
                 </div>
 
                 <div className={Styles.Information_Box}>
-                  <div className={Styles.Top}>
-                    <h3 className={Styles.Title}>{value.Title}</h3>
-                    <p className={Styles.Title}>{value.Description}</p>
+                  {/* <div className={Styles.Top}> */}
+                  <h3 className={Styles.Project_Title}>{value.Title}</h3>
+                  <p className={Styles.Description}>{value.Description}</p>
 
-                    <div className={Styles.Stack_Container}>
-                      <div className={Styles.Stack}>{value.Stacks.Stack_1}</div>
-                      <div className={Styles.Stack}>{value.Stacks.Stack_2}</div>
-                      <div className={Styles.Stack}>{value.Stacks.Stack_3}</div>
-                      <div className={Styles.Stack}>{value.Stacks.Stack_4}</div>
-                      <div className={Styles.Stack}>{value.Stacks.Stack_5}</div>
-                      <div className={Styles.Stack}>{value.Stacks.Stack_6}</div>
-                    </div>
+                  <div className={Styles.Stack_Container}>
+                    <div className={Styles.Stack}>{value.Stacks.Stack_1}</div>
+                    <div className={Styles.Stack}>{value.Stacks.Stack_2}</div>
+                    <div className={Styles.Stack}>{value.Stacks.Stack_3}</div>
+                    <div className={Styles.Stack}>{value.Stacks.Stack_4}</div>
+                    <div className={Styles.Stack}>{value.Stacks.Stack_5}</div>
+                    <div className={Styles.Stack}>{value.Stacks.Stack_6}</div>
                   </div>
+                  {/* </div> */}
 
                   <div className={Styles.Bottom}>
-                    <CodeIcon sx={{ fontSize: 30, color: "white" }} />
-                    <HttpIcon sx={{ fontSize: 30, color: "white" }} />
+                    <GitHubIcon sx={{ fontSize: 40, color: "white" }} />
+                    <LaunchIcon sx={{ fontSize: 40, color: "white" }} />
                   </div>
                 </div>
               </div>
