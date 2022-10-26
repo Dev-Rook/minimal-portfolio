0b9ebf078357d01f758f9dbadc47a4f9b6c58112
Dev Rook <110010478+Dev-Rook@users.noreply.github.com>
2022-10-26 00:53:54 -0500

    Commit

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import Styles from "../Styles/Page-Styles/Contact/Contact.module.css"
+import React from "react";
+import Styles from "../Styles/Page-Styles/Contact/Contact.module.css";
 
 const Contact = () => {
   return (
     <div className={Styles.Page}>
-        Contact
-        <div className={Styles.Content_Container}>
-
-        </div>
+      Contact
+      <div className={Styles.Content_Container}>
+        <form action="" className={Styles.Form}></form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
---
+++ src/Pages/useAxios.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect, useCallback } from "react";
+import axios from "axios";
+
+const useAxios = (url) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const getApiData = useCallback(async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get(url);
+      setData(response.data);
+      console.log(response.data);
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  }, [url]);
+
+  useEffect(() => {
+    getApiData();
+  }, [getApiData]);
+
+  return { data, loading, error };
+};
+
+export default useAxios;
